95d73d249ba2d5f74d2f748d3ef045ef66872516
Dan Ursoviciu <dan.ursoviciu@gmail.com>
2015-05-12 12:03:58 +0200

    add bundle services list to container parameter bag

--- DependencyInjection/VagrafonyExtension.php
+++ DependencyInjection/VagrafonyExtension.php
@@ -22,6 +22,8 @@ class VagrafonyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('vagrafony.services', $config['install_services']);
+
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
