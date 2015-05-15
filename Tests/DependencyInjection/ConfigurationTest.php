1b3c6e63f9433f1a6cf44a0136362f0b91c98d82
Dan Ursoviciu <dan.ursoviciu@gmail.com>
2015-05-15 10:59:59 +0200

    configuration test file and stub method added

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danursoviciu
+ * Date: 15/05/15
+ * Time: 10:18
+ */
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Configuration test
+     */
+    public function testGetConfigTreeBuilder()
+    {
+        $this->assertTrue(true);
+    }
+
+}
